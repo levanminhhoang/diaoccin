bbc5f8875d3cabdf0660ae5a8faf6118ced70338
hoang <hoang.levan@snaptec.co>
2022-08-13 22:05:59 +0700

    update data

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { auth, db } from "../firebase/config";
-import { BiMessageDetail } from "react-icons/bi";
+
 import { collection, onSnapshot } from "firebase/firestore";
-import { signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+
 const token = 1;
 
 const Admin = () => {
