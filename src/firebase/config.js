7906eede6c5ddeaad28d168714934fd9ded8b697
hoang <hoang.levan@snaptec.co>
2022-08-13 19:42:13 +0700

    update icon

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyA0TTdnjqVHg-fU0Wi5ZLm6C9TIgBBpDbU",
-  authDomain: "kimquy-4e78c.firebaseapp.com",
-  projectId: "kimquy-4e78c",
-  storageBucket: "kimquy-4e78c.appspot.com",
-  messagingSenderId: "914545086195",
-  appId: "1:914545086195:web:dad37df66254ea4ddcf186",
-  measurementId: "G-WT58GQQQCP",
+  apiKey: "AIzaSyDYaJ_yLkxkCGwTjWq-_DF2A92tpjqT8J4",
+  authDomain: "diaoccin.firebaseapp.com",
+  projectId: "diaoccin",
+  storageBucket: "diaoccin.appspot.com",
+  messagingSenderId: "987575611574",
+  appId: "1:987575611574:web:1b4adac477f83f3f987617",
 };
 
 // Initialize Firebase
