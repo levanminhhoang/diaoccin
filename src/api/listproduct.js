826c0d6559ad47ada3ce321f270c3a37e2d440d6
ngocnhu <ngocnhu04032000@gmail.com>
2022-08-14 18:39:06 +0530

    data

--- src/api/listproduct.js
+++ src/api/listproduct.js
@@ -1,7 +1,7 @@
 export const project = [
   {
     id: 1,
-    image: "/project/test.jpg",
+    image: "",
     thumbnail: [
       {
         id: 1,
@@ -16,28 +16,23 @@ export const project = [
         thumbnail: "",
       },
     ],
-    title: "Bán căn hộ chung cư Linh Tây 155m2, 3PN, đã có sổ hồng",
-    address: "Đường D1, Phường Linh Tây, TP. Thủ Đức, TP. Hồ Chí Minh",
+    title: "",
+    address: "Kiệt Đặng Văn Ngữ- Huế",
     price: {
-      value: 38.5,
-      current: "tỷ",
+      value: 3.4,
+      current: "Tỷ",
     },
-    exchangeRate: 25.68,
     infoProject: {
-      area: 155,
-      longs: 15.5,
-      width: 10,
-      direction: "",
-      bedroom: 3,
-      bathroom: 2,
+      area: 118.4,
+      street: 3,
+      direction: "ĐB",
     },
-    slug: "ban-can-ho-chung-cu-linh",
-    Introduce:
-      "Chính chủ cần bán căn hộ chung cư phòng B304, diện tích 155m2, thông thủy 150m2. Nhà đầy đủ tiện ích, còn rất mới, dọn vào ở ngay.Xung quanh đầy đủ tiện ích, cách Phạm Văn Đồng và Kha Vạn Cân 300m. Nhà còn rất mới.",
+    slug: "",
+    Introduce: "",
   },
   {
-    id: 1,
-    image: "/project/test.jpg",
+    id: 2,
+    image: "",
     thumbnail: [
       {
         id: 1,
@@ -52,28 +47,23 @@ export const project = [
         thumbnail: "",
       },
     ],
-    title: "Bán căn hộ chung cư Linh Tây 155m2, 3PN, đã có sổ hồng",
-    address: "Đường D1, Phường Linh Tây, TP. Thủ Đức, TP. Hồ Chí Minh",
+    title: "",
+    address: "Kiêt Dương Thiệu Tước-Thủy dương- Hương Thuỷ",
     price: {
-      value: 38.5,
-      current: "tỷ",
+      value: 1.35,
+      current: "Tỷ",
     },
-    exchangeRate: 25.68,
     infoProject: {
-      area: 155,
-      longs: 15.5,
-      width: 10,
-      direction: "",
-      bedroom: 3,
-      bathroom: 2,
+      area: 81.2,
+      street: 2,
+      direction: "TN",
     },
-    slug: "ban-can-ho-chung-cu-linh",
-    Introduce:
-      "Chính chủ cần bán căn hộ chung cư phòng B304, diện tích 155m2, thông thủy 150m2. Nhà đầy đủ tiện ích, còn rất mới, dọn vào ở ngay.Xung quanh đầy đủ tiện ích, cách Phạm Văn Đồng và Kha Vạn Cân 300m. Nhà còn rất mới.",
+    slug: "",
+    Introduce: "",
   },
   {
-    id: 1,
-    image: "/project/test.jpg",
+    id: 3,
+    image: "",
     thumbnail: [
       {
         id: 1,
@@ -88,28 +78,23 @@ export const project = [
         thumbnail: "",
       },
     ],
-    title: "Bán căn hộ chung cư Linh Tây 155m2, 3PN, đã có sổ hồng",
-    address: "Đường D1, Phường Linh Tây, TP. Thủ Đức, TP. Hồ Chí Minh",
+    title: "",
+    address: "Kiệt 111 Đặng Văn Ngữ- Huế",
     price: {
-      value: 38.5,
-      current: "tỷ",
+      value: 4.6,
+      current: "Tỷ",
     },
-    exchangeRate: 25.68,
     infoProject: {
-      area: 155,
-      longs: 15.5,
-      width: 10,
-      direction: "",
-      bedroom: 3,
-      bathroom: 2,
+      area: 108,
+      street: 4.7,
+      direction: "T",
     },
-    slug: "ban-can-ho-chung-cu-linh",
-    Introduce:
-      "Chính chủ cần bán căn hộ chung cư phòng B304, diện tích 155m2, thông thủy 150m2. Nhà đầy đủ tiện ích, còn rất mới, dọn vào ở ngay.Xung quanh đầy đủ tiện ích, cách Phạm Văn Đồng và Kha Vạn Cân 300m. Nhà còn rất mới.",
+    slug: "",
+    Introduce: "",
   },
   {
-    id: 1,
-    image: "/project/test.jpg",
+    id: 4,
+    image: "",
     thumbnail: [
       {
         id: 1,
@@ -124,28 +109,23 @@ export const project = [
         thumbnail: "",
       },
     ],
-    title: "Bán căn hộ chung cư Linh Tây 155m2, 3PN, đã có sổ hồng",
-    address: "Đường D1, Phường Linh Tây, TP. Thủ Đức, TP. Hồ Chí Minh",
+    title: "",
+    address: "110 Phan Bội Châu- Phường Trường An- TP Huế",
     price: {
-      value: 38.5,
-      current: "tỷ",
+      value: 7.6323,
+      current: "Tỷ",
     },
-    exchangeRate: 25.68,
     infoProject: {
-      area: 155,
-      longs: 15.5,
-      width: 10,
-      direction: "",
-      bedroom: 3,
-      bathroom: 2,
+      area: 195.7,
+      street: 10,
+      direction: "ĐN",
     },
-    slug: "ban-can-ho-chung-cu-linh",
-    Introduce:
-      "Chính chủ cần bán căn hộ chung cư phòng B304, diện tích 155m2, thông thủy 150m2. Nhà đầy đủ tiện ích, còn rất mới, dọn vào ở ngay.Xung quanh đầy đủ tiện ích, cách Phạm Văn Đồng và Kha Vạn Cân 300m. Nhà còn rất mới.",
+    slug: "",
+    Introduce: "",
   },
   {
-    id: 1,
-    image: "/project/test.jpg",
+    id: 5,
+    image: "",
     thumbnail: [
       {
         id: 1,
@@ -160,23 +140,483 @@ export const project = [
         thumbnail: "",
       },
     ],
-    title: "Bán căn hộ chung cư Linh Tây 155m2, 3PN, đã có sổ hồng",
-    address: "Đường D1, Phường Linh Tây, TP. Thủ Đức, TP. Hồ Chí Minh",
+    title: "",
+    address: "41/15B Lịch Đợi - Phường Đúc- Huế",
     price: {
-      value: 38.5,
-      current: "tỷ",
+      value: 3,
+      current: "Tỷ",
     },
-    exchangeRate: 25.68,
     infoProject: {
-      area: 155,
-      longs: 15.5,
-      width: 10,
-      direction: "",
-      bedroom: 3,
-      bathroom: 2,
+      area: 138,
+      street: 3.4,
+      direction: "N",
     },
-    slug: "ban-can-ho-chung-cu-linh",
-    Introduce:
-      "Chính chủ cần bán căn hộ chung cư phòng B304, diện tích 155m2, thông thủy 150m2. Nhà đầy đủ tiện ích, còn rất mới, dọn vào ở ngay.Xung quanh đầy đủ tiện ích, cách Phạm Văn Đồng và Kha Vạn Cân 300m. Nhà còn rất mới.",
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 6,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Tổ 5 - KV2 - P.An Đông Huế",
+    price: {
+      value: 3.675,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 147,
+      street: 3.5,
+      direction: "T",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 7,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "69/2 Bà Triệu- P.Phú Hội-TP Huế",
+    price: {
+      // value: 3.675,
+      current: "TL",
+    },
+    infoProject: {
+      area: 51,
+      street: 5.2,
+      direction: "N",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 8,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Tổ 10- KV4-P.Thuỷ Xuân-Huế",
+    price: {
+      value: 2.1,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 164,
+      street: 3.65,
+      direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 9,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Tổ 10- KV4-P.Thuỷ Xuân-Huế",
+    price: {
+      value: 2.3,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 115.1,
+      street: 3.86,
+      direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 10,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "TDP Ngọc Anh- Phú Thượng Huế",
+    price: {
+      value: 2.75,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 100.7,
+      street: 7.5,
+      direction: "ĐB",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 11,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Thôn Phú Khê- Phú Dương TP Huế",
+    price: {
+      value: 1.35,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 100,
+      street: 3,
+      direction: "B",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 12,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Xã Thuỷ Bằng- Tp Huế",
+    price: {
+      value: 4.167,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 694.5,
+      street: 3,
+      direction: "TB",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 13,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Xã Thuỷ Bằng- Tp Huế",
+    price: {
+      value: 2.478,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 413,
+      street: 3,
+      direction: "ĐB",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 14,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Xã Lộc Vĩnh - Phú Lộc ",
+    price: {
+      value: 3.3,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 546.9,
+      street: 6.4,
+      // direction: "ĐB",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 15,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Thị Xã Hương Thuỷ ",
+    price: {
+      value: 920,
+      current: "Triệu",
+    },
+    infoProject: {
+      area: 80,
+      street: 4.12,
+      // direction: "ĐB",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 16,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Khu tái định cư Lộc Sơn-Phú Lộc",
+    price: {
+      value: 1.5,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 193.5,
+      street: 11.5,
+      // direction: "ĐB",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 17,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Thôn Thuận Hoá-Lộc Bổn-Phú Lộc",
+    price: {
+      value: 1.1,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 212,
+      street: 5,
+      // direction: "ĐB",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 18,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "TỔ 10- P.An Tây TP.Huế",
+    price: {
+      value: 6.7116,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 1677.9,
+      street: 3,
+      direction: "T",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 19,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Tổ 5 KV2- P.An Đông TP Huế",
+    price: {
+      value: 7.5,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 180,
+      street: 3,
+      direction: "N",
+    },
+    slug: "",
+    Introduce: "",
+  },
+  {
+    id: 20,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Dạ Lê Chánh - Thủy Vân TP HUẾ",
+    price: {
+      value: 1.45,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 80,
+      street: 3.2,
+      direction: "TB",
+    },
+    slug: "",
+    Introduce: "",
   },
 ];
