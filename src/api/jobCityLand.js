99108cf56518998bc3034ba45f05a62eace9cbd2
ngocnhu <ngocnhu04032000@gmail.com>
2022-08-15 13:02:36 +0700

    API city house

---
+++ src/api/jobCityLand.js
@@ -0,0 +1,386 @@
+export const project = [
+  {
+    id: 1,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "MT Nguyễn Khoa Vy- Vỹ Dạ- Tp Huế",
+    price: {
+      value: 7.5,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 130.3,
+      street: 5,
+      direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 2,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "MT Ngọc Anh - Phú Thượng- Huế",
+    price: {
+      value: 6.2,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 170,
+      street: 11,
+      direction: "N",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 3,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Số 4 Đường Cao Xuân Huy - Vỹ Dạ- Thành Phố Huế",
+    price: {
+      value: 7.5,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 115.8,
+      street: 12,
+      direction: "T",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 4,
+    image: "/nhatp/aphinh.jpg",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "/nhatp/aphinh1.jpg",
+      },
+      {
+        id: 2,
+        thumbnail: "/nhatp/aphinh2.jpg",
+      },
+      {
+        id: 3,
+        thumbnail: "/nhatp/aphinh3.jpg",
+      },
+    ],
+    title: "",
+    address: "30 Kiệt 246 Hùng Vương-P.An Cựu- Huế",
+    price: {
+      value: 4,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 60.4,
+      street: 3.5,
+      direction: "B",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 5,
+    image: "/nhatp/cthanh.jpg",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "/nhatp/cthanh1.jpg",
+      },
+      {
+        id: 2,
+        thumbnail: "/nhatp/cthanh2.jpg",
+      },
+      {
+        id: 3,
+        thumbnail: "/nhatp/cthanh3.jpg",
+      },
+    ],
+    title: "",
+    address: "298 Phan Chu Trinh- An Cựu Huế",
+    price: {
+      value: 2.35,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 52.3,
+      street: 3,
+      direction: "Đ",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 6,
+    image: "/nhatp/cty.jpg",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "/nhatp/cty1.jpg",
+      },
+      {
+        id: 2,
+        thumbnail: "/nhatp/cty2.jpg",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Lô D1- KĐT Mỹ Thượng- Xã Phú Mỹ - Huyện Phú Vang",
+    price: {
+      value: 12.5,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 215.6,
+      street: 36,
+      direction: "N-TN",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 7,
+    image: "/nhatp/phuthuong.jpg",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "/nhatp/phuthuong1.jpg",
+      },
+      {
+        id: 2,
+        thumbnail: "/nhatp/phuthuong2.jpg",
+      },
+      {
+        id: 3,
+        thumbnail: "/nhatp/phuthuong3.jpg",
+      },
+    ],
+    title: "",
+    address: "La Ỷ - Phú Thượng - TP Huế",
+    price: {
+      value: 2.25,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 75.3,
+      street: 3,
+      direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 8,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Nhà mặt tiền 4 tầng tỉnh lộ 10 (mới hoàn thiện)",
+    price: {
+      value: 7.45,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 115.8,
+      //   street: 3,
+      //   direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 9,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "Biệt thự thô 3 tầng",
+    price: {
+      value: 7.1,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 252,
+      street: 13.5,
+      //   direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 10,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "nhà mặt tiền 3 tầng Phạm Văn Đồng",
+    price: {
+      value: "12,x",
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 122.4,
+      street: 13.5,
+      //   direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 11,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "31 Xóm Gióng- P.An Tây",
+    price: {
+      value: 3.2,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 81,
+      street: 4,
+      direction: "T-TB",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+  {
+    id: 12,
+    image: "",
+    thumbnail: [
+      {
+        id: 1,
+        thumbnail: "",
+      },
+      {
+        id: 2,
+        thumbnail: "",
+      },
+      {
+        id: 3,
+        thumbnail: "",
+      },
+    ],
+    title: "",
+    address: "70 Phạm THị Liên- Phường Kim Long- Huế",
+    price: {
+      value: 5.5,
+      current: "Tỷ",
+    },
+    infoProject: {
+      area: 106.8,
+      street: 14,
+      direction: "TN",
+    },
+    slug: "",
+    Introduce: "",
+    //không có ảnh
+  },
+];
